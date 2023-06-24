3bebab60211a3ef2b36c5af17340b9ba4f4d1d6e
Nykuluz <carlmillanes3@gmail.com>
2023-06-24 10:59:22 +0800

    new variable

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        int x = 1;
-        int y = 2;
+    public static void main(String[] args) {
+        System.out.print("Hello World");
     }
 }
